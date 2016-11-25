b9c4f8b6a415c0d75edd41fe152d1af2b86c4bd2
Ujash Joshi <ujash.joshi@utoronto.ca>
2016-11-24 19:22:40 -0500

    Fixing white spaces and variable name shadowing function name.

--- neuralnet.py
+++ neuralnet.py
@@ -1,5 +1,4 @@
 import time
-import random
 import tensorflow as tf
 
 
@@ -19,20 +18,19 @@ class LayeredNetwork:
 
         if not rseed:
             rseed = time.time()
-        random.seed(rseed)
 
         hid_layer_num = 0
         for layer_width, activation_func in hid_layers:
             with tf.name_scope("Layer" + str(hid_layer_num)) as scope:
                 self.tensors["layer" + str(hid_layer_num)] = self.layer(prevl_width, prev_layer,
                                                                         layer_width, hid_layer_num,
-                                                                        activation_func, scope, snapshot)
+                                                                        activation_func, scope, snapshot, rseed)
             prevl_width = layer_width
             prev_layer = "layer" + str(hid_layer_num)
-            hid_layer_num+=1
+            hid_layer_num += 1
 
         with tf.name_scope("OutputLayer") as scope:
-            self.outputs = self.output_layer(prevl_width, prev_layer, y_width, scope, snapshot)
+            self.outputs = self.output_layer(prevl_width, prev_layer, y_width, scope, snapshot, rseed)
 
         with tf.name_scope("Training") as scope:
             self.labels = tf.placeholder(tf.int32, shape=(batch_size), name="Labels")
@@ -43,13 +41,13 @@ class LayeredNetwork:
             self.testy = tf.placeholder(tf.int32, [None, ], name="Test_y")
             self.acc = self.accuracy()
 
-    def layer(self, prevl_width, prev_layer, layer_width, layer_num, activation_func, scope, snapshot):
+    def layer(self, prevl_width, prev_layer, layer_width, layer_num, activation_func, scope, snapshot, rseed):
         if snapshot:
             wl = snapshot["w" + str(layer_num)]
             bl = snapshot["b" + str(layer_num)]
         else:
-            wl = tf.random_normal([prevl_width, layer_width], dtype=tf.float32, stddev=1e-1)
-            bl = tf.random_normal([layer_width], dtype=tf.float32, stddev=1e-1)
+            wl = tf.random_normal([prevl_width, layer_width], dtype=tf.float32, stddev=1e-1, seed=rseed)
+            bl = tf.random_normal([layer_width], dtype=tf.float32, stddev=1e-1, seed=rseed)
 
         self.parameters["w" + str(layer_num)] = tf.Variable(wl, name="weight")
         self.parameters["b" + str(layer_num)] = tf.Variable(bl, name="bias")
@@ -58,13 +56,13 @@ class LayeredNetwork:
         #layer1 = tf.nn.tanh(layer1l)
         return activation_func(layer_l, name="activation")
 
-    def output_layer(self, prevl_width, prev_layer, y_width, scope, snapshot):
+    def output_layer(self, prevl_width, prev_layer, y_width, scope, snapshot, rseed):
         if snapshot:
             wl = snapshot["wOutput"]
             bl = snapshot["bOutput"]
         else:
-            wl = tf.random_normal([prevl_width, y_width], dtype=tf.float32, stddev=1e-1)
-            bl = tf.random_normal([y_width], dtype=tf.float32, stddev=1e-1)
+            wl = tf.random_normal([prevl_width, y_width], dtype=tf.float32, stddev=1e-1, seed=rseed)
+            bl = tf.random_normal([y_width], dtype=tf.float32, stddev=1e-1, seed=rseed)
 
         self.parameters["wOutput"] = tf.Variable(wl, name="weight")
         self.parameters["bOutput"] = tf.Variable(bl, name="bias")
@@ -75,8 +73,8 @@ class LayeredNetwork:
     def training(self, y_):
         entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(self.outputs, tf.to_int64(y_))
         cost = tf.reduce_mean(entropy)
-        return tf.train.AdamOptimizer(learning_rate=self.constants["learning_rate"]).minimize(cost,
-                                                                                        global_step=self.global_step)
+        return tf.train.AdamOptimizer(learning_rate=self.constants["learning_rate"]).minimize(
+            cost, global_step=self.global_step)
 
     def accuracy(self):
         self.tensors["testy"] = self.testy
